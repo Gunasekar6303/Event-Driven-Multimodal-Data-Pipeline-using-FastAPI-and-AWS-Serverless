e31339e32c1962c90efcbfb7da55a6124b2d903a
Gunasekar <131892608+Gunasekar6303@users.noreply.github.com>
2026-03-15 11:41:10 +0530

    Added files via direct upload option in the github

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, UploadFile, File
+import boto3
+import uuid
+
+app = FastAPI()
+
+BUCKET_NAME = "practice-bucket-for-lambda"
+
+s3 = boto3.client("s3")
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    file_id = str(uuid.uuid4())
+    key = f"uploads/{file_id}_{file.filename}"
+
+    contents = await file.read()
+
+    s3.put_object(
+        Bucket = BUCKET_NAME,
+        Key = key,
+        Body = contents
+    )
+
+    return{
+        "filename": key,
+        "message": "File uploaded successfully"
+    }
